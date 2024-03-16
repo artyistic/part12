99d532335c9d76fd251d0f5045881d3b73e6c33c
dhleung <artyistic@gmail.com>
2024-03-16 16:52:32 -0500

    12.14: Testing during the build process

---
+++ todo-app/todo-frontend/src/Todos/Todo.js
@@ -0,0 +1,34 @@
+import List from "./List"
+
+const Todo = ({ todo, onClickDelete, onClickComplete }) => {
+  const doneInfo = (
+    <>
+      <span data-testid="status">This todo is done</span>
+      <span>
+        <button data-testid="deleteButton" onClick={onClickDelete(todo)}> Delete </button>
+      </span>
+    </>
+  )
+
+  const notDoneInfo = (
+    <>
+      <span data-testid="status">
+        This todo is not done
+      </span>
+      <span>
+        <button data-testid="deleteButton" onClick={onClickDelete(todo)}> Delete </button>
+        <button data-testid="completeButton" onClick={onClickComplete(todo)}> Set as done </button>
+      </span>
+    </>
+  )
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
+            <span data-testid="todoText">
+              {todo.text} 
+            </span>
+            {todo.done ? doneInfo : notDoneInfo}
+          </div>
+  )
+}
+
+export default Todo
---
+++ todo-app/todo-frontend/src/tests/todo.test.js
@@ -0,0 +1,58 @@
+import { render, screen, cleanup } from "@testing-library/react";
+import Todo from "../Todos/Todo";
+import { useState } from "react";
+// afterEach function runs after each test suite is executed
+afterEach(() => {
+  cleanup(); // Resets the DOM after each test suite
+});
+
+describe("Button Component", () => {
+  const mockDelete = jest.fn();
+  const mockComplete = jest.fn();
+
+  test("Button Text", () => {
+    const testTodo = {
+      text: "jest test",
+      done: false
+    }
+    render(<Todo todo={testTodo} onClickComplete={mockComplete} onClickDelete={mockDelete}/>);
+    const status = screen.getByTestId("status");
+    expect(status).toHaveTextContent("This todo is not done");
+  });
+
+  test("Button complete", () => {
+    const testTodo = {
+      text: "jest test",
+      done: false
+    }
+    render(<Todo todo={testTodo} onClickComplete={mockComplete} onClickDelete={mockDelete}/>);
+    const completeButton = screen.getByTestId("completeButton")
+    // const deleteButton = screen.getByTestId("deleteButton")
+    const status = screen.getByTestId("status");
+
+    // const todoText = screen.getByTestId("todoText")
+    expect(completeButton).toHaveTextContent("Set as done");
+  })
+
+  test("Button complete", () => {
+    const testTodo = {
+      text: "jest test",
+      done: false
+    }
+    render(<Todo todo={testTodo} onClickComplete={mockComplete} onClickDelete={mockDelete}/>);
+    const deleteButton = screen.getByTestId("deleteButton")
+    expect(deleteButton).toHaveTextContent("Delete");
+  })
+
+  test("Button done==true", () => {
+    const testTodo = {
+      text: "jest test",
+      done: true
+    }
+    render(<Todo todo={testTodo} onClickComplete={mockComplete} onClickDelete={mockDelete}/>);
+    const deleteButton = screen.getByTestId("deleteButton")
+    const completeButton = screen.queryByTestId("completeButton")
+    expect(deleteButton).toHaveTextContent("Delete");
+    expect(completeButton).not.toBeInTheDocument();
+  })
+});
